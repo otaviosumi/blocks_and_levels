89ad81f4fe7b623633ebd03aa6d1dbbe2ededf9c
otaviosumi <sumi@DESKTOP-NSCPBP3.localdomain>
2019-11-01 23:00:01 -0300

    Starting point

---
+++ tap_n_pull/Assets/MeshGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+
+    public Material mat;
+
+    float width = 1;
+    float height = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[4];
+
+        vertices[0] = new Vector3(-width, -height);
+        vertices[1] = new Vector3(-width, height);
+        vertices[2] = new Vector3(width, height);
+        vertices[3] = new Vector3(width, -height);
+
+        mesh.vertices = vertices;
+
+        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+
+        GetComponent<MeshRenderer>().material = mat;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
